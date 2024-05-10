6d72305cbec6f71cc459d5e15fd26bf11b778f1c
etmoji <itansanchez1@gmail.com>
2024-05-10 14:17:41 -0600

    Creacion modelo pedido

---
+++ models/pedido.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+  customerName: { type: String, required: true },
+  totalPrice: { type: Number, required: true },
+  products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }],
+  player: { type: mongoose.Schema.Types.ObjectId, ref: 'Player' },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+});
+
+const Order = mongoose.model('Order', orderSchema);
+
+export default Order;
